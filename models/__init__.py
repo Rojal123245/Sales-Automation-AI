a26f6bddc9b588055ef7f272cfef0d9027236978
rojalpradhan <rozalpra@gmail.com>
2025-02-03 13:08:18 -0500

    Fixing the error after modularization

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 from .preprocess import DataPreprocessor
-from .train import ModelTrainer
 from .evaluate import ModelEvaluator
 
-__all__ = ['DataPreprocessor', 'ModelTrainer', 'ModelEvaluator']
+__all__ = ['DataPreprocessor', 'ModelEvaluator']
